0281e93945c2bea03fb0204512627c0875d45deb
DENNYYESSAR <dennisokeri5@gmail.com>
2023-11-14 09:33:53 -0800

    printf_rot.c

--- printf_rot.c
+++ printf_rot.c
@@ -1,38 +1,68 @@
 #include "main.h"
 
 /**
- * printf_rot - rot13 conversion
+ * printf_rot - Handles custom conversion specifiers including 'R'.
  * @args: arguments
- * Return: val
+ * Return: Number of characters printed
  */
-
 int printf_rot(va_list args)
 {
         int i, j, val = 0;
         int k = 0;
         char *str = va_arg(args, char *);
-        char a [] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-        char b [] = {"qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"};
+        char a[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+        char b[] = {"qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"};
 
         if (str == NULL)
                 str = "(null)";
-        for (i = 0; str[i] != '\0'; i++)
+
+        // Check if the custom specifier is 'R'
+        if (str[0] == '%' && str[1] == 'R')
         {
-                k = 0;
-                for (j = 0; a[j] && !k; j++)
+                // Move the pointer to the actual string
+                str += 2;
+
+                for (i = 0; str[i] != '\0'; i++)
                 {
-                        if (str[i] == a[j])
+                        k = 0;
+                        for (j = 0; a[j] && !k; j++)
                         {
-                                _putchar(b[j]);
+                                if (str[i] == a[j])
+                                {
+                                        _putchar(b[j]);
+                                        val++;
+                                        k = 1;
+                                }
+                        }
+                        if (!k)
+                        {
+                                _putchar(str[i]);
                                 val++;
-                                k = 1;
                         }
                 }
-                if (!k)
+        }
+        else
+        {
+                // Handle the regular rot13 conversion
+                for (i = 0; str[i] != '\0'; i++)
                 {
-                        _putchar(str[i]);
-                        val++;
+                        k = 0;
+                        for (j = 0; a[j] && !k; j++)
+                        {
+                                if (str[i] == a[j])
+                                {
+                                        _putchar(b[j]);
+                                        val++;
+                                        k = 1;
+                                }
+                        }
+                        if (!k)
+                        {
+                                _putchar(str[i]);
+                                val++;
+                        }
                 }
         }
+
         return (val);
 }
