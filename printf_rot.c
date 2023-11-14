873b99b7641535116b620d38a00d18abb29f84de
DENNYYESSAR <dennisokeri5@gmail.com>
2023-11-14 09:42:03 -0800

    printf_rot.c

--- printf_rot.c
+++ printf_rot.c
@@ -1,68 +1,37 @@
 #include "main.h"
 
 /**
- * printf_rot - Handles custom conversion specifiers including 'R'.
+ * printf_rot - rot13 conversion
  * @args: arguments
- * Return: Number of characters printed
+ * Return: val
  */
 int printf_rot(va_list args)
 {
-        int i, j, val = 0;
-        int k = 0;
-        char *str = va_arg(args, char *);
-        char a[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-        char b[] = {"qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"};
+	int i, j, val = 0;
+	int k = 0;
+	char *str = va_arg(args, char *);
+	char a[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char b[] = {"qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"};
 
-        if (str == NULL)
-                str = "(null)";
-
-        // Check if the custom specifier is 'R'
-        if (str[0] == '%' && str[1] == 'R')
-        {
-                // Move the pointer to the actual string
-                str += 2;
-
-                for (i = 0; str[i] != '\0'; i++)
-                {
-                        k = 0;
-                        for (j = 0; a[j] && !k; j++)
-                        {
-                                if (str[i] == a[j])
-                                {
-                                        _putchar(b[j]);
-                                        val++;
-                                        k = 1;
-                                }
-                        }
-                        if (!k)
-                        {
-                                _putchar(str[i]);
-                                val++;
-                        }
-                }
-        }
-        else
-        {
-                // Handle the regular rot13 conversion
-                for (i = 0; str[i] != '\0'; i++)
-                {
-                        k = 0;
-                        for (j = 0; a[j] && !k; j++)
-                        {
-                                if (str[i] == a[j])
-                                {
-                                        _putchar(b[j]);
-                                        val++;
-                                        k = 1;
-                                }
-                        }
-                        if (!k)
-                        {
-                                _putchar(str[i]);
-                                val++;
-                        }
-                }
-        }
-
-        return (val);
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		k = 0;
+		for (j = 0; a[j] && !k; j++)
+		{
+			if (str[i] == a[j])
+			{
+				_putchar(b[j]);
+				val++;
+				k = 1;
+			}
+		}
+		if (!k)
+		{
+			_putchar(str[i]);
+			val++;
+		}
+	}
+	return (val);
 }
