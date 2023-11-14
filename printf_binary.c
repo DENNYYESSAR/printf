c23421043e38e85ca41ab1c5a8e30d19bf08c482
DENNYYESSAR <dennisokeri5@gmail.com>
2023-11-14 08:19:28 -0800

    printf_binary.c

--- printf_binary.c
+++ printf_binary.c
@@ -1,36 +1,35 @@
 #include "main.h"
 
 /**
- * print_binary - converts int to bi
+ * print_binary - converts unsigned int to binary
  * @args: arguments
- * Return: the binary number
+ * Return: the number of binary digits printed
  */
 
 int print_binary(va_list args)
 {
-	int lag = 0;
-	int digt = 0;
-	unsigned int j;
-	int i, a = 1, k;
-	unsigned int fig = va_arg(args, unsigned int);
+    int lag = 0;
+    int digt = 0;
+    unsigned int j;
+    int i, a = 1, k;
+    unsigned int fig = va_arg(args, unsigned int);
 
-	for (i = 0; i < 32; i++)
-	{
-		j = ((a << (32 - i)) & fig);
-		if (j >> (31 - i))
-			lag = 1;
-		if (lag)
-		{
-			k = j >> (31 - i);
-			_putchar(k + 48);
-			digt++;
-		}
-	}
-	if (digt == 0)
-	{
-		digt++;
-		_putchar('0');
-	}
-	return (digt);
-
+    for (i = 0; i < 32; i++)
+    {
+        j = ((a << (32 - i)) & fig);
+        if (j >> (31 - i))
+            lag = 1;
+        if (lag)
+        {
+            k = j >> (31 - i);
+            _putchar(k + '0');
+            digt++;
+        }
+    }
+    if (digt == 0)
+    {
+        digt++;
+        _putchar('0');
+    }
+    return (digt);
 }
